75aa9b50864774e7b3e25fb7de15d2ad500c2264
lev0607 <lev36rus@gmail.com>
2020-06-01 21:53:31 +0300

    created controller, model and migrate for Task

---
+++ database/migrations/2020_06_01_165413_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('status_id')->default(1)->references('id')->on('task_statuses');
+            $table->integer('created_by_id')->references('id')->on('users');
+            $table->integer('assigned_to_id')->nullable()->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
--- tests/Feature/TaskStatusControllerTest.php
+++ tests/Feature/TaskStatusControllerTest.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-
 class TaskStatusControllerTest extends TestCase
 {
     public function testIndex()

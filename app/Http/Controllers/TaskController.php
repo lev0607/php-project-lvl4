6efee8974a3b002cbde5c707e8bb26482eb432ec
lev0607 <lev36rus@gmail.com>
2020-06-27 00:26:34 +0300

    fix bug

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -90,6 +90,8 @@ class TaskController extends Controller
 
     public function destroy(Task $task)
     {
+        $this->authorize('delete', $task);
+
         $this->service->delete($task);
 
         flash(__('flash.task_delete'))->success();
